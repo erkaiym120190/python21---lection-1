40e62cfd9c2e219a121a6771f40bf69d5b2c3c5c
Erkaiym <erkaiym.urkunchieva.ch@gmail.com>
2022-06-23 12:02:09 +0600

    встроенные функции

---
+++ functions/game.py
@@ -0,0 +1,49 @@
+# a = str(4331)
+
+# for i in a:
+#     if (a[-1]) < (a[-2]) and (a[-2]) < (a[-3]) and (a[-3]) < (a[-4]):
+#         print('yes')
+#     else:
+#         print('no')
+
+
+# a = 4311
+# b = list(str(a))
+# if b == b.sort():
+#     print('Yes')
+# el
+
+# a = int(input())
+# b = list(str(a))
+# b.sort()
+# c = list(str(a))
+# if b == c:
+#     print('yes')
+# else:
+#     print('no')
+
+
+# a, b, c = 0, 2, 5
+# def new_chisla():
+#     print(a + b)
+#     print(c - b)
+#     print(a + b + c)
+# new_chisla()
+
+
+
+# a = 0
+# b = 2
+# c = 5
+
+# a1 = a + b
+# b1 = c - b
+# c = a + b + c
+
+# a = a1
+# b = b1
+# print(a ,b, c)
+
+# a, b, c = 0, 2, 5
+# a, b, c = a + b, c - b, a + b + c
+# print(a, b, c)
